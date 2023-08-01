8b3113ff59f562deb4557b72ebc05361d8e84c88
Andrey <antonov.andrey.10101@gmail.com>
2023-08-01 13:55:01 +0400

    feat: add Header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Layout, Space } from 'antd';
 import MainPage from '../../pages/MainPage/MainPage';
 import CoursesPage from '../../pages/CoursesPage/CoursesPage';
 import ErrorPage from '../../pages/ErrorPage/ErrorPage';
 import './App.scss';
 
+const { Header, Content, Footer } = Layout;
+
 function App() {
   return (
     <Router>
+      <Header style={{ display: 'flex', alignItems: 'center', height: 50 }}>
+        <Space>
+          <Link to="/">Главная </Link>
+          <Link to="/courses">Курсы </Link>
+        </Space>
+      </Header>
       <div className="app">
         <Routes>
           <Route path="/" element={<MainPage />} />
